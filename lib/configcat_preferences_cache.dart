216119442830c53903f53af810910ba4c4d542fb
Peter Csajtai <peter.csajtai@outlook.com>
2024-10-21 11:31:55 +0200

    Fix analyze warnings

--- lib/configcat_preferences_cache.dart
+++ lib/configcat_preferences_cache.dart
@@ -2,6 +2,6 @@
 ///
 /// Cache implementation based on shared_preferences for ConfigCat Dart (Flutter) SDK. ConfigCat is a hosted feature flag service that lets you manage feature toggles across frontend, backend, mobile, desktop apps.
 
-library configcat_preferences_cache;
+library;
 
 export 'src/configcat_preferences_cache.dart';
